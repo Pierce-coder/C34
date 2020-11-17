723979b535c372bacb3fc3d0f18dffa5a2fb60e5
Pierce-coder <pierce_olsen@icloud.com>
2020-11-16 20:14:21 -0500

    Create dataBase, read/write

--- sketch.js
+++ sketch.js
@@ -1,24 +1,36 @@
 var ball;
 
+//location (ball) --- ref()
+//read --- on()
+//write ---- set()
+
+var database;
+var ballPosition;
+
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
+    ball = createSprite(10,10,10,10);
     ball.shapeColor = "red";
+
+    database = firebase.database();
+    ballPosition =  database.ref("ball/position");
+    ballPosition.on("value", readPosition, showError);
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
@@ -26,4 +38,21 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;   
+}
+
+function showError(){
+    console.log("Oops , Some Error!!")
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x:position.x+x, 
+        y:position.y+y
+    });
 }
